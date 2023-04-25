2f48286638203e9a871aeec381920d99c8f456a2
Angelo Morada <angelomorada@gmail.com>
2023-04-25 14:02:24 +0800

    [Feature 1] added username column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_055544) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_060150) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_055544) do
     t.integer "coins", default: 0
     t.decimal "total_deposit", precision: 18, scale: 2, default: "0.0"
     t.integer "children_members", default: 0
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
