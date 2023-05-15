9fa13090103faec8f23ea59e3dd18c8bcefe2f8d
Angelo <angelomorada@gmail.com>
2023-05-16 07:24:45 +0800

    [Feature 1] updated admin and client routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
 
-  root "home#index"
+  constraints(ClientDomainConstraint.new) do
+    devise_for :users, controllers: { sessions: 'client/sessions', registrations: 'client/registrations' } 
+    namespace :client, path: '' do
+      root 'home#index'
+      resource :home
+    end
+  end
+
+  constraints(AdminDomainConstraint.new) do
+    devise_for :users, controllers: { sessions: 'admin/sessions' }
+    namespace :admin, path: '' do
+      root 'home#index'
+      resource :home
+    end
+  end
 end
