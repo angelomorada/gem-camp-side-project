28d2109963b2273d309994c462f2e4156c1e8033
Angelo <angelomorada@gmail.com>
2023-05-16 08:29:20 +0800

    [Feature 1] added login restrictions for admin and client

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   constraints(ClientDomainConstraint.new) do
+    get '/', to: 'client/home#index'
     devise_for :users, controllers: { sessions: 'client/sessions', registrations: 'client/registrations' } 
     namespace :client, path: '' do
       root 'home#index'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   end
 
   constraints(AdminDomainConstraint.new) do
+    get '/', to: 'admin/home#index'
     devise_for :users, controllers: { sessions: 'admin/sessions' }
     namespace :admin, path: '' do
       root 'home#index'
